4802fcfd2ccf746a04f7adc90d3cc2c2d8f5e4eb
waylight3 <waylight3@naver.com>
2017-11-28 21:50:18 +0900

    add view

--- all/views.py
+++ all/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.db.models import Q
+from datetime import datetime, date, timezone
+from ipware.ip import get_ip
+import html, difflib, json
+from django.core.mail import send_mail
+from django.http import JsonResponse
 
-# Create your views here.
+def index(request):
+	data = {
+		'name':'hi',
+	}
+	return render(request, 'all/index.html', data)
--- gk/urls.py
+++ gk/urls.py
@@ -17,7 +17,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from all import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', views.index),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
